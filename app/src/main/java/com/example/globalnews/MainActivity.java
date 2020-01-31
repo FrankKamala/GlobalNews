f69676ba63981f648022083f8a247a80cfc664ba
FrankKamala <frankkamala@gmail.com>
2020-01-31 16:45:57 +0300

    display Title searched

--- app/src/main/java/com/example/globalnews/MainActivity.java
+++ app/src/main/java/com/example/globalnews/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.globalnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.edOne) EditText mTitle;
+    @BindView(R.id.search) Button mSearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = mTitle.getText().toString();
+                Intent intent = new Intent(MainActivity.this, News.class);
+                intent.putExtra("Title",title);
+
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/globalnews/News.java
+++ app/src/main/java/com/example/globalnews/News.java
@@ -2,13 +2,24 @@ package com.example.globalnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class News extends AppCompatActivity {
+    @BindView(R.id.textView) TextView mOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("Title");
+        mOutput.setText("This articles were posted By:"+title+".");
     }
 }
