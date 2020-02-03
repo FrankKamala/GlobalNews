f0a52e9d27cd4bd6d1e5cfbe1b9ac65e54ba7d7f
FrankKamala <frankkamala@gmail.com>
2020-02-03 08:46:02 +0300

    extend Array adapter and add constructor

--- app/src/main/java/com/example/globalnews/NewsAdapter.java
+++ app/src/main/java/com/example/globalnews/NewsAdapter.java
@@ -1,4 +1,19 @@
 package com.example.globalnews;
 
-public class NewsAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class NewsAdapter extends ArrayAdapter {
+    private String[] mTitle;
+    private String[] mContent;
+    private Context mContext;
+
+    public NewsAdapter(Context mContext, int resource,String[] mTitle,String [] mContent){
+        super(mContext,resource);
+        this.mContext =mContext;
+        this.mTitle =mTitle;
+        this.mContent =mContent;
+
+
+    }
 }
