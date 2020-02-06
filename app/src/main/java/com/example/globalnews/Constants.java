1e36967c37d6c0b242b2e9f5760c28ad47a0d3e1
FrankKamala <frankkamala@gmail.com>
2020-02-06 05:26:05 +0300

    add my Constants

--- app/src/main/java/com/example/globalnews/Constants.java
+++ app/src/main/java/com/example/globalnews/Constants.java
@@ -1,4 +1,7 @@
 package com.example.globalnews;
 
 public class Constants {
+
+    public static final String NEWS_BASE_URL = "https://newsapi.org/v2/";
+    public static final String API_KEY = BuildConfig.API_KEY;
 }
