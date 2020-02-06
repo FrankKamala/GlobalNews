f5efa8bc7f8a47cfd91c8857f2f34880ff5a2e13
FrankKamala <frankkamala@gmail.com>
2020-02-06 11:58:11 +0300

    add GeT Request onEndpoint and Query params

--- app/src/main/java/com/example/globalnews/NewsApi.java
+++ app/src/main/java/com/example/globalnews/NewsApi.java
@@ -1,7 +1,15 @@
 package com.example.globalnews;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface NewsApi {
+    @GET("top-headlines")
+    Call<NewsSearchResponse>getNews(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
 
 }
