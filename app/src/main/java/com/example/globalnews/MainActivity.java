463027f917bb82d2c64806b4e5316a1c0b02567d
FrankKamala <frankkamala@gmail.com>
2020-02-06 16:33:42 +0300

    add frame layout and card

--- app/src/main/java/com/example/globalnews/MainActivity.java
+++ app/src/main/java/com/example/globalnews/MainActivity.java
@@ -12,26 +12,26 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.edOne) EditText mTitle;
-    @BindView(R.id.search) Button mSearch;
+//    @BindView(R.id.edOne) EditText mTitle;
+//    @BindView(R.id.search) Button mSearch;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        mSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String titLe = mTitle.getText().toString();
-                Intent intent = new Intent(MainActivity.this, News.class);
-                intent.putExtra("Title",titLe);
-
-                startActivity(intent);
-
-
-            }
-        });
+//        ButterKnife.bind(this);
+//        mSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String titLe = mTitle.getText().toString();
+//                Intent intent = new Intent(MainActivity.this, News.class);
+//                intent.putExtra("Title",titLe);
+//
+//                startActivity(intent);
+//
+//
+//            }
+//        });
     }
 }
