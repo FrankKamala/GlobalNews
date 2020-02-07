82a7973a5f2b698779abbcb7c4ea626621544392
FrankKamala <frankkamala@gmail.com>
2020-02-07 10:04:05 +0300

    XTND recycler View and set click listener

--- app/src/main/java/com/example/globalnews/Adapter.java
+++ app/src/main/java/com/example/globalnews/Adapter.java
@@ -1,4 +1,26 @@
 package com.example.globalnews;
 
-public class Adapter {
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter {
+    private List <Article> articles;
+    private Context context ;
+    private OnItemClickListener onItemClickListener;
+
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener{
+        void onItemClickListener (View view, int position);
+    }
+
 }
