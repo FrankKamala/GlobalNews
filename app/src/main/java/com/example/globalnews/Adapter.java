7159e43037adc97cf21463328c80ffdf34f7bb6e
FrankKamala <frankkamala@gmail.com>
2020-02-07 10:12:39 +0300

    Implement onCreate ViewHolder Bind and getItem Count method

--- app/src/main/java/com/example/globalnews/Adapter.java
+++ app/src/main/java/com/example/globalnews/Adapter.java
@@ -2,8 +2,13 @@ package com.example.globalnews;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -19,8 +24,39 @@ public class Adapter extends RecyclerView.Adapter {
         this.onItemClickListener = onItemClickListener;
     }
 
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public interface OnItemClickListener{
         void onItemClickListener (View view, int position);
+    }
+
+    public class MHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView title,desc,author,published_ad,source,time;
+        ImageView imageView;
+        ProgressBar progressBar;
+        OnItemClickListener onItemClickListener;
+        public MHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 
 }
