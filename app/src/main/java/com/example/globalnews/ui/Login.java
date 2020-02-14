8f4bcdd805abebf84d8fa70aa184ef7db0b71b2f
FrankKamala <frankkamala@gmail.com>
2020-02-14 15:51:17 +0300

    Linear

--- app/src/main/java/com/example/globalnews/ui/Login.java
+++ app/src/main/java/com/example/globalnews/ui/Login.java
@@ -3,14 +3,23 @@ package com.example.globalnews.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.globalnews.R;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
