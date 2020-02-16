1fc2d309fd8346370f77999261455325bb0dfb71
FrankKamala <frankkamala@gmail.com>
2020-02-16 21:31:34 +0300

    implement onClick

--- app/src/main/java/com/example/globalnews/ui/CreateAccount.java
+++ app/src/main/java/com/example/globalnews/ui/CreateAccount.java
@@ -3,14 +3,20 @@ package com.example.globalnews.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.globalnews.R;
 
-public class CreateAccount extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
