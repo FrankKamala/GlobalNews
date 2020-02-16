3fcf8684e3992dfd603186d0133f2f1079144d47
FrankKamala <frankkamala@gmail.com>
2020-02-16 21:46:23 +0300

    add auth and signup logic

--- app/src/main/java/com/example/globalnews/ui/CreateAccount.java
+++ app/src/main/java/com/example/globalnews/ui/CreateAccount.java
@@ -1,22 +1,93 @@
 package com.example.globalnews.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.globalnews.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
+    EditText mail,pass;
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        mail = (EditText) findViewById(R.id.email_signup);
+        pass = (EditText) findViewById(R.id.key);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+    }private void registerUser(){
+        String email=mail.getText().toString().trim();
+        String password=pass.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            mail.setError("Email is required");
+            mail.requestFocus();
+            return;
+        }
+
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mail.setError("Please enter a valid email");
+            mail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            pass.setError("Password is required");
+            pass.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            pass.setError("Minimum length of password should be 6");
+            pass.requestFocus();
+            return;
+        }
+
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"User registered",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Account Details Exist ",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
 
+            case R.id.textViewLogin:
+                finish();
+                startActivity(new Intent(CreateAccount.this, Login.class));
+                break;
+        }
     }
 }
