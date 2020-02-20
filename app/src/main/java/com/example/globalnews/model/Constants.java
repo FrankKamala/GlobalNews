7d34f16cb32c9de27c6279aad95e07e47676eee9
FrankKamala <frankkamala@gmail.com>
2020-02-20 09:16:24 +0300

    add newsChild const

--- app/src/main/java/com/example/globalnews/model/Constants.java
+++ app/src/main/java/com/example/globalnews/model/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
 
     public static final String NEWS_BASE_URL = "https://newsapi.org/v2/";
     public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String FIREBASE_CHILD_NEWS = "news";
 }
