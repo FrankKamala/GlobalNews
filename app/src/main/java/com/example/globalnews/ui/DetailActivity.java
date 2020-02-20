a8ce80b1365a395295fc35355de3456b5d49b6f5
FrankKamala <frankkamala@gmail.com>
2020-02-20 09:40:14 +0300

    add save Functionality on Specified news

--- app/src/main/java/com/example/globalnews/ui/DetailActivity.java
+++ app/src/main/java/com/example/globalnews/ui/DetailActivity.java
@@ -3,8 +3,10 @@ package com.example.globalnews.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -12,6 +14,9 @@ import androidx.viewpager.widget.ViewPager;
 import com.example.globalnews.R;
 import com.example.globalnews.adapters.Adapter;
 import com.example.globalnews.model.Article;
+import com.example.globalnews.model.Constants;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -33,7 +38,7 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     @BindView(R.id.title_on_appbar) TextView source;
     @BindView(R.id.title) TextView mTitle;
     @BindView(R.id.newsImage) ImageView mImage;
-
+    @BindView(R.id.saveNews) Button mSave;
 
 
 
@@ -41,13 +46,16 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     private Adapter viewAdapter;
     List<Article> mNews;
     private Context mContext;
+    int itemPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         mNews = Parcels.unwrap(getIntent().getParcelableExtra("article"));
-        int itemPos = getIntent().getIntExtra("position", 0);
+
+        itemPos = getIntent().getIntExtra("position", 0);
+
 
 
 //        viewAdapter = new Adapter(mNews, mContext);
@@ -55,6 +63,7 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
 //        mViewPager.setCurrentItem(itemPos);
 //
         ButterKnife.bind(this);
+        mSave.setOnClickListener(this);
 
         Picasso.get().load(mNews.get(itemPos).getUrlToImage()).into(mImage);
 
@@ -72,6 +81,16 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
+
+        if (v == mSave) {
+            DatabaseReference newsRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FIREBASE_CHILD_NEWS);
+            newsRef.push().setValue(mNews.get(itemPos));
+            Toast.makeText(DetailActivity.this, "Saved For Later", Toast.LENGTH_SHORT).show();
+
+
+        }
 
     }
 }
