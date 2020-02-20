ba060d3ccddf23cc54a9883b1316f4b68ba83fde
FrankKamala <frankkamala@gmail.com>
2020-02-20 09:15:20 +0300

    display save this

--- app/src/main/java/com/example/globalnews/ui/DetailActivity.java
+++ app/src/main/java/com/example/globalnews/ui/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.globalnews.ui;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.viewpager.widget.ViewPager;
 import com.example.globalnews.R;
 import com.example.globalnews.adapters.Adapter;
 import com.example.globalnews.model.Article;
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
@@ -18,16 +21,19 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
 
 
 
-//    @BindView(R.id.date) TextView date;
+//    @BindView(R.id.date) TextView date;title_on_appbar
     @BindView(R.id.time) TextView time;
     @BindView(R.id.descrip) TextView mDescription;
 
     @BindView(R.id.author) TextView author;
+    @BindView(R.id.title_on_appbar) TextView source;
     @BindView(R.id.title) TextView mTitle;
+    @BindView(R.id.newsImage) ImageView mImage;
+
 
 
 
@@ -50,9 +56,11 @@ public class DetailActivity extends AppCompatActivity {
 //
         ButterKnife.bind(this);
 
+        Picasso.get().load(mNews.get(itemPos).getUrlToImage()).into(mImage);
 
         time.setText(mNews.get(itemPos).getPublishedAt());
         author.setText(mNews.get(itemPos).getAuthor());
+
         mTitle.setText(mNews.get(itemPos).getTitle());
         mDescription.setText(mNews.get(itemPos).getDescription());
 
@@ -62,4 +70,8 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
