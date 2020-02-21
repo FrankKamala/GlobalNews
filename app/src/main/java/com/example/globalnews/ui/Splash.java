80fb3f75d546bbb916422afd38206f02a79f55ef
FrankKamala <frankkamala@gmail.com>
2020-02-21 11:39:56 +0300

    anim to activity class

--- app/src/main/java/com/example/globalnews/ui/Splash.java
+++ app/src/main/java/com/example/globalnews/ui/Splash.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.globalnews.R;
 
+
+
 public class Splash extends AppCompatActivity {
+
+    Animation topAnim;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.up_anim);
 
 
         Handler handler =new Handler();
